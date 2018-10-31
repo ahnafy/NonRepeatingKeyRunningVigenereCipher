1b8b7e9dd8ad0b26f595a95646284db31d82500d
Khondoker Yasin Ahnaf Prio <yasin011@morris.umn.edu>
2018-10-31 18:58:28 -0500

    Update trim.sh

--- etc/trim.sh
+++ etc/trim.sh
@@ -2,6 +2,7 @@
 
 NAME="$1_report.txt";
 
+# This function reduces all the words with '.' in it as we are condisering word with no period only of importance to us
 grep -v '\.' "$1_report.txt" > "$1 trimmed.txt"; rm "$1_report.txt"
 
 
